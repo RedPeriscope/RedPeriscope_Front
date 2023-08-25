7d8493b270c0751e609a5511e1dfe4c921d2b931
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-08-26 01:29:18 +0900

    전체보기 페이지네이션 적용

--- src/Routers.js
+++ src/Routers.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./page/Main";
+import Allplan from "./page/Allplan";
 
 const Routers = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Main/>} />
+			<Route path="/all" element={<Allplan />} />
 		</Routes>
 	);
 };
